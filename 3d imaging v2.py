1fd5f939c4ebac33bb7a2967e858a89892f0850d
asteel982 <73165972+asteel982@users.noreply.github.com>
2021-01-25 14:28:56 +0000

    Update 3d imaging v2.py
    New version of code now projecting using tkinter however isnt plotting the 3d image

--- 3d imaging v2.py
+++ 3d imaging v2.py
@@ -2,50 +2,58 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import time
+import tkinter as tk
+from PIL import Image, ImageTk
+import time
+
 
 cam = cv2.VideoCapture(0)
 
+def sinewave(resX,resY,period,phase):
+    x = np.arange(0, resX)
+  
+    ons  = np.ones([resY,1])
+    x_2 = np.matmul(ons,[x])
+
+    angfreq = 2*np.pi/period
+    y  = np.cos(angfreq*x_2 + phase)
+    return y
 
 
 
 phase = [0, 2*np.pi/3, 4*np.pi/3]
 period = 50 #Sinewave period
-#setting x and y values for the fringe patterns according to the resolution of the projector
-Xres = 854 
-Yres = 480 
+resX = 1920 #Size in x direction
+resY = 1080 #Size in y direction
 
 
-# Get x values and the sine wave
-def sinewave(Xres,period,phase):
-    x = np.arange(0, Xres)
-    ons  = np.ones([Yres,1])
-    x_2 = np.matmul(ons,[x])
 
-    angfreq = 2*np.pi/period
-    #I1 = Ixy +(IIxy(cos(phi(x)+phsae))) 
-    y  = np.cos(angfreq*x_2+phase)
-    return y
+
+root = tk.Tk()
+
+root.geometry("1920x1080+1910+0") 
+
+canvas = tk.Canvas(root,width=1920,height=1080)
+canvas.pack()
 
 frame = {}
-for i,phs in enumerate(phase):
-    frame[i] = sinewave(Xres,period,phs)
-fig=plt.figure()
-fig.canvas.manager.full_screen_toggle()
- # toggle fullscreen mode
-k = cv2.waitKey(1)
-#displaying the fringe patterns in a loop with time set by plt.pause however the usual command to close this loop with a keystroke does not seem to be working
-while True:
-    for i in range(3):
-        
-        plt.imshow(frame[i],cmap='gray', vmin=-1, vmax=1)
-        plt.yticks([])
-        plt.xticks([])
-        plt.tight_layout(pad=0, w_pad=0, h_pad=0)
-        plt.pause(1)
-        ret,frameOut = cam.read()
-        frame[i] = np.sum(frameOut,axis=2)
 
-time.sleep(5)
+
+for a,phs in enumerate(phase):
+    array = sinewave(resX, resY,period,phs)
+    array = array*128 + 127 
+    
+    img =  ImageTk.PhotoImage(Image.fromarray(array))
+    canvas.create_image(0, 0, anchor="nw", image=img)
+    
+    root.update() # Show plot
+    ret,frameOut = cam.read()
+    frame[a] = np.sum(frameOut,axis=2)
+    time.sleep(1.0)    # Pause 
+
+root.destroy()
+
+
 
 depth = np.arctan2(np.sqrt(3)*(frame.get(0)-frame.get(2)),2*frame.get(1)-frame.get(0)-frame.get(2))
 depth_uw = np.unwrap(depth)
@@ -55,11 +63,9 @@ ax = fig.add_subplot(111, projection='3d')
 y = np.arange(0,480,1)
 x = np.arange(0,640,1)
 X, Y = np.meshgrid(x, y)
-#zs = np.array(fun(np.ravel(X), np.ravel(Y)))
 Z = depth_uw.reshape(X.shape)
 
 ax.plot_surface(X, Y, Z)
-   
     
    
    
