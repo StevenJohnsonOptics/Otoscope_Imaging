3ff619c395dc5af5fb09a41c019731007ea5c15d
asteel982 <73165972+asteel982@users.noreply.github.com>
2021-02-01 17:16:01 +0000

    Update 3d imaging v2.py

--- 3d imaging v2.py
+++ 3d imaging v2.py
@@ -6,7 +6,26 @@ import tkinter as tk
 from PIL import Image, ImageTk
 import time
 from mpl_toolkits.mplot3d import Axes3D
+cam = cv2.VideoCapture(0)
+cv2.namedWindow("test")
+img_counter = 0
+#captures and displays video which allows alignment before taking image
+while True:
+    ret, frame = cam.read()
+    if not ret:
+        print("failed to grab frame")
+        break
+    cv2.imshow("test", frame)
 
+    k = cv2.waitKey(1)
+    if k%256 == 27:
+        # closes it if ESC pressed
+        print("Escape hit, closing...")
+        break
+    
+cam.release()
+cv2.destroyAllWindows()
+#%%
 cam = cv2.VideoCapture(0)
 
 def sinewave(resX,resY,period,phase):
@@ -52,7 +71,7 @@ for a,phs in enumerate(phase):
     ret,frameOut = cam.read()
     frame[a] = np.sum(frameOut,axis=2)/(3*255)
     disp_image = Image.fromarray(frameOut)
-    filename = r'c:\Users\user\Desktop\Frame_tests\Mug3\Frame_'+str(a)+'.png'
+    filename = r'c:\Users\user\Desktop\Frame_tests\mic\Frame_'+str(a)+'.png'
     disp_image.save(filename)
 root.destroy()
 
@@ -69,3 +88,35 @@ X, Y = np.meshgrid(x, y)
 Z = depth_uw.reshape(X.shape)
 
 ax.plot_surface(X, Y, Z)
+
+#%%
+fringe = {}
+for i in range(3):
+    fringe[i] = cv2.imread(r'c:\Users\user\Desktop\Frame_tests\Mug2\Frame_'+str(i)+'.png')
+    frame[i] = np.asarray(fringe[i])
+    frame[i] = frame[i].astype(np.float32)
+    frame[i] = np.sum(frame[i],axis=2)
+
+depth = np.arctan2(np.sqrt(3)*(frame.get(0)-frame.get(2)),2*frame.get(1)-frame.get(0)-frame.get(2))
+depth_uw = np.unwrap(depth)
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+y = np.arange(0,480,1)
+x = np.arange(0,640,1)
+X, Y = np.meshgrid(x, y)
+z = depth_uw.reshape(X.shape)
+
+valid = z>0
+Z = z[valid]
+X = X[valid]
+Y = Y[valid]
+
+ax.scatter(X,Y,Z)
+ax.set_xlabel('X Axes')
+ax.set_ylabel('Y Axes')
+ax.set_zlabel('Z Axes')
+
+
+
+plt.show()
